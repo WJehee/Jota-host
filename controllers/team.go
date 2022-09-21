0f0184db15920f70a1dea0dff297de48310bdd82
Wouter Jehee <wouterjehee@tutanota.com>
2022-09-21 15:24:20 +0200

    add registration and password checking for teams

--- controllers/team.go
+++ controllers/team.go
@@ -1,30 +1,58 @@
 package controllers
 
-import(
-    "net/http"
+import (
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "github.com/wjehee/jota-host/models"
+    "golang.org/x/crypto/bcrypt"
+	"github.com/gin-gonic/gin"
+	"github.com/wjehee/jota-host/models"
 )
 
 // Register a new team
 func Register(c *gin.Context) {
-    team := models.Team{
-        Username: "bob",
-        Password: "test",
+    var team models.Team;
+    err := c.Bind(&team)
+    if err != nil {
+        c.String(http.StatusBadRequest, err.Error())
+    } else {
+        // hash the password of the user before inserting into the database
+        hashed_pw, err := bcrypt.GenerateFromPassword([]byte(team.Password), 10)
+        if err != nil {
+            c.Status(http.StatusInternalServerError)
+        }
+        team.Password = string(hashed_pw)
+        result := models.DB.FirstOrCreate(&team)
+        if result.RowsAffected == 0 {
+            c.Status(http.StatusUnprocessableEntity)
+        } else {
+            c.JSON(http.StatusCreated, team)
+        }
     }
-    models.DB.Create(&team)
-    c.JSON(http.StatusCreated, team)
 }
 
 // Login
 func Login(c *gin.Context) {
-
+    var login struct {
+        Username string
+        Password string
+    }
+    err := c.Bind(&login)
+    if err != nil {
+        c.String(http.StatusBadRequest, err.Error())
+    }
+    var team models.Team
+    models.DB.First(&team, "username = ?", login.Username)
+    err = bcrypt.CompareHashAndPassword([]byte(team.Password), []byte(login.Password))
+    if err != nil {
+        c.Status(http.StatusForbidden)
+    } else {
+        // TODO: actually login
+        c.Status(http.StatusOK)
+    }
 }
 
 func GetTeam(c *gin.Context) {
     var team models.Team
     models.DB.First(&team, "username = ?", "bob")
-    c.JSON(http.StatusOK, team)
 }
 
--- models/team.go
+++ models/team.go
@@ -1,7 +1,7 @@
 package models
 
 type Team struct {
-    Username string `json:"username" binding:"min=2,max=32,required"`
+    Username string `json:"username" binding:"min=2,max=32,required" gorm:"primaryKey"`
     Password string `json:"password" binding:"min=8,max=128,required"`
     // Teamname string `json:"teamname" binding:"min=2, max=32"`
     // Points int64
